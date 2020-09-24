e3eed27bea7beb6a2ac87efa2bec5612d047b3e7
Bao Nguyen <btn6364@rit.edu>
2020-09-24 19:19:17 -0400

    Update methods to prove anagrams

--- String/anagram.py
+++ String/anagram.py
@@ -1,4 +1,5 @@
-def is_anagram(st1, st2):
+def is_anagram_arr(st1, st2):
+    #Use cound array
     #O(L + A) runtime | O(A) space. 
     if len(st1) != len(st2):
         return False
@@ -12,7 +13,24 @@ def is_anagram(st1, st2):
             return False
     return True
 
+from collections import defaultdict
+def is_anagram_hash(st1, st2):
+    if len(st1) != len(st2):
+        return False
+    freq_map = defaultdict(int)
+    for ch1, ch2 in zip(st1, st2):
+        freq_map[ch1] += 1
+        freq_map[ch2] -= 1
+
+    for ch in freq_map:
+        if freq_map[ch] != 0:
+            return False
+    return True
+
+
+
 if __name__ == '__main__':
     st1 = "binary"
-    st2 = "braisy"
-    print(f"Is anagram: {is_anagram(st1, st2)}")
+    st2 = "brainy"
+    print(f"Is anagram: {is_anagram_arr(st1, st2)}")
+    print(f"Is anagram: {is_anagram_hash(st1, st2)}")
